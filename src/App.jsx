3a829e03b4239ab81a72d0009a30f0bdd21f7ce6
Timothy Laubscher <timlaubscher5063@gmail.com>
2026-03-11 15:15:55 -0400

    show details function added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import { useState } from "react";
+import { tvShows } from "./shows/data";
+import EpisodeList from "./episodes/EpisodeList";
+import ShowSelection from "./shows/ShowSelection";
+import EpisodeDetails from "./episodes/EpisodeDetails";
+import ShowDetails from "./shows/ShowDetails";
+
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState(null);
   return (
     <>
       <header>
         <p>React TV</p>
       </header>
-      <main></main>
+      <main>
+        <ShowSelection showData={tvShows} setSelectedShow={setSelectedShow} />
+      </main>
     </>
   );
 }
--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,22 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({
+  showData,
+  selectedShow,
+  setSelectedShow,
+}) {
+  return (
+    <nav className="shows">
+      {showData.map((show) => (
+        <a
+          className={"show" + (show === selectedShow ? " selected" : "")}
+          key={show.name}
+          onClick={() => setSelectedShow(show)}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
